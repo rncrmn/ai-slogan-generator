14eb0a26fdf1b6bfbb467b0893c1a8ee20ad4d0d
rncrmn <aaronleadgeex@gmail.com>
2023-03-13 11:45:38 +0800

    repsonsive

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 const App: Component = () => {
   return (
-    <div class="ts-generator h-screen w-screen flex flex-col justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-amber-200 via-violet-600 to-sky-900">
+    <div class="ts-generator h-screen w-screen flex flex-col justify-center items-center bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-amber-200 via-violet-600 to-sky-900 px-5">
       <div class="ts-generator__container max-w-[500px] w-full bg-white py-8 px-4 md:py-12 md:px-8 shadow-md rounded-lg">
         <h1 class="text-2xl md:text-4xl font-bold text-center mb-8 md:mb-10">
           Free Slogan Generator
